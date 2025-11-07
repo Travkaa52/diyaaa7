5bfb6d7fd0957401c635a307bcae81062788c67c
Travkaa52 <dimakasan028@gmail.com>
2025-11-07 17:17:53 +0200

    Refactor App component with UserDataProvider and fonts
    Updated the App component to include UserDataProvider and font configuration.

--- mollydiya-main/pages/_app.js
+++ mollydiya-main/pages/_app.js
@@ -1,6 +1,24 @@
-import '../styles/globals.css'
-import React from 'react'
+// pages/_app.js (обновленный)
+
+import '@/styles/globals.css';
+import { Inter } from 'next/font/google'; 
+import { UserDataProvider } from '../components/UserDataContext'; // <-- ИМПОРТ ПРОВАЙДЕРА
+
+// ... (Ваш код конфигурации шрифта) ...
+
+const inter = Inter({
+  subsets: ['latin', 'cyrillic'], 
+  variable: '--font-inter',
+});
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${inter.variable} font-sans`}>
+      {/* 💡 Оборачиваем все страницы в провайдер данных */}
+      <UserDataProvider> 
+        <Component {...pageProps} />
+      </UserDataProvider>
+    </div>
+  );
 }
